32be7e08be5e132ab45be61a8026589197fd5a98
diegocfreitas-tech <diegocfreitas@gmail.com>
2026-03-09 14:24:13 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
